d847185e8307ee5b7468ab8ede097430d2b527f3
aynaitlamine <aynaitlamine.dev@gmail.com>
2022-09-13 17:40:42 +0100

    initia commit

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<script>
+import { QuillEditor } from "@vueup/vue-quill";
+
+export default {
+	components: {
+		QuillEditor,
+	},
+	data() {
+		return {
+			editorOption: {},
+			content: "<h2>I am Example</h2>",
+		};
+	},
+	methods: {
+		handleDrop(e) {
+			const txt = e.dataTransfer.getData("Text");
+
+			const quil = this.$refs.myEditor.getQuill();
+			var selection = quil.getSelection(true);
+			quil.insertText(selection.index, txt);
+			console.log(quil);
+		},
+		handleTest() {
+			console.log("test");
+		},
+	},
+};
+</script>
+<template>
+	<div @dragover.prevent @drop.prevent>
+		<quill-editor
+			ref="myEditor"
+			content-type="html"
+			:options="editorOption"
+			v-model:content="content"
+			@drop="handleDrop"
+			@focus="handleTest">
+		</quill-editor>
+	</div>
+	<div>hello</div>
+	<div>hi</div>
+</template>
+<style></style>
